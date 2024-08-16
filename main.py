02f1d9731ba57359d2098ee75382870ca8788b1d
Mino <BW1MtXl1j4prQQg@proton.me>
2024-08-17 00:20:09 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+import discord, json, time
+from discord.ext import commands
+from colorama import Fore
+
+with open("./data/config.json", "r") as f:
+    config = json.load(f)
+
+bot = commands.Bot(command_prefix=config["prefix"], help_command=None, self_bot=True)
+
+@bot.event
+async def on_ready():
+    print(f"[Logged in as {bot.user}]\nLatency: {bot.latency*1000}ms")
+
+@bot.command()
+async def purge(ctx, amount: int=None, limit=None):
+    if amount and limit is None:
+        await ctx.reply("message purge command.\nusage: -purge [amount] [float(time)]")
+    messages = [message async for message in ctx.channel.history(limit=amount + 1)]
+    for msg in messages:
+        if msg.author == bot.user:
+            try:
+                await msg.delete()
+                print(Fore.RED+"[DELETED]"+Fore.RESET+f" {msg.author} | {msg.content}")
+                time.sleep(float(limit))
+            except discord.errors.Forbidden as e:
+                if e.code == 50021: 
+                    pass
+    await ctx.send(f"`deleted`", delete_after=5)
+
+bot.run(config["token"])
