034ca310a5a7337cfb1964cbf290ac894f545a5b
M8 <BW1MtXl1j4prQQg@proton.me>
2024-08-20 01:14:19 +0900

    Update main.py

--- main.py
+++ main.py
@@ -13,18 +13,21 @@ async def on_ready():
 
 @bot.command()
 async def purge(ctx, amount: int=None, limit=None):
-    if amount and limit is None:
+    if amount is None and limit is None:
         await ctx.reply("message purge command.\nusage: -purge [amount] [float(time)]")
+    count = 0
     messages = [message async for message in ctx.channel.history(limit=amount + 1)]
     for msg in messages:
         if msg.author == bot.user:
             try:
                 await msg.delete()
+                count += 1
                 print(Fore.RED+"[DELETED]"+Fore.RESET+f" {msg.author} | {msg.content}")
                 time.sleep(float(limit))
             except discord.errors.Forbidden as e:
                 if e.code == 50021: 
                     pass
     await ctx.send(f"`deleted`", delete_after=5)
+    print(Fore.GREEN+"[FINISHED]"+Fore.RESET+f" Count: {count}")
 
 bot.run(config["token"])
